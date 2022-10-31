81781258fbba4e9e531cfd85c7ccb1b3a282e481
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-31 10:23:36 +0100

    created the Navbar file

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -50,10 +50,18 @@ const Navbar = (props: Props) => {
         )}
 
         {toggleMenu && (
-          <ul>
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
+
+            {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, i) => (
+              <NavbarItem
+                key={item + i}
+                title={item}
+                classProps="my-2 text-lg"
+              />
+            ))}
           </ul>
         )}
       </div>
