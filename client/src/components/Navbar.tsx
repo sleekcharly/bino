bf2bed33071ec36abbfadcc86c7dd0c621610a4f
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-31 09:26:07 +0100

    added gitignore file

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+type NavbarProps = {
+  title: string;
+  classProps?: string;
+};
+
+const NavbarItem = ({ title, classProps }: NavbarProps) => (
+  <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>
+);
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, i) => (
+          <NavbarItem key={item + i} title={item} />
+        ))}
+
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
+        </li>
+      </ul>
+
+      <div className="flex relative">
+        {toggleMenu ? (
+          <AiOutlineClose
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <HiMenuAlt4
+            fontSize={28}
+            className="text-white md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+
+        {toggleMenu && (
+          <ul>
+            <li className="text-xl w-full my-2">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
