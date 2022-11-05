d9389029ade73215459a30000ad1fd778d6c088f
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-11-05 04:40:08 +0100

    setup hardhat.config file

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+// https://eth-goerli.g.alchemy.com/v2/o16-wYH2ptBLizQYlO6c7FpjX28GQZ1x
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+import '@nomiclabs/hardhat-waffle';
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/o16-wYH2ptBLizQYlO6c7FpjX28GQZ1x',
+      accounts: [
+        '91bcff4cdf6c5b9b8c99add1d110c330279f0845d5472ff5474e65011bc0076e',
+      ],
+    },
+  },
 };
-
-export default config;
--- smart_contract/scripts/deploy.ts
+++ smart_contract/scripts/deploy.ts
@@ -1,23 +1,25 @@
-import { ethers } from "hardhat";
+// Deploying our transactions to the blockchain
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+import { ethers } from 'hardhat';
 
-  const lockedAmount = ethers.utils.parseEther("1");
+const main = async () => {
+  const Transactions = await ethers.getContractFactory('Transactions');
+  const transactions = await Transactions.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await transactions.deployed();
 
-  await lock.deployed();
+  console.log(`Transactions deployed to: ${transactions.address}`);
+};
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-}
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0); // which means the process executed successfully
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
+  }
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// execute the main function
+runMain();
