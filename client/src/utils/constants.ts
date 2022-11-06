30367f2cd3559913bcc3d0d644a4064997b81c62
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-11-06 23:39:02 +0100

    setup structure for interacting with etherum smart contract

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,8 @@
+// contract abi from json file
+import abi from './Transactions.json';
+
+// contract abi
+export const contractABI = abi.abi;
+
+// contract address
+export const contractAddress = '0x06cF70E059c81E13C2Eca3fB928871AbB18f79ea';
